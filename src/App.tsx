c2a87b28fc931bf5fd71c52e917e2a7aa4d4042f
Yure <yurecod@gmail.com>
2024-07-09 15:45:04 -0400

    last update

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ThemeProvider } from 'styled-components'
+import { Outlet } from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Header } from './components/Header'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import { CartContextProvider } from './contexts/CartProvider'
 
+export function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
+      <CartContextProvider>
+        <Header />
+        <Outlet />
+      </CartContextProvider>
+    </ThemeProvider>
   )
 }
-
-export default App
